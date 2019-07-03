af78901029c938b7e5a49164e6fa3116cc689b79
karlosgarciap <karlos@fidenet.net>
2019-07-03 13:27:43 +0200

    Change selectors in other projects

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-webapp',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
