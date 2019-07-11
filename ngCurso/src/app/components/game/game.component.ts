a18bcdf9226a95b4b9403a5e1c53ff61a191ddd8
karlosgarciap <karlos@fidenet.net>
2019-07-11 12:27:11 +0200

    Refresh quiz question

--- ngCurso/src/app/components/game/game.component.ts
+++ ngCurso/src/app/components/game/game.component.ts
@@ -10,48 +10,53 @@ import {PaisService} from "../../pais.service";
 })
 export class GameComponent implements OnInit {
 
-  pais: any = {};
-  answers: any[] = [];
-  numberOfQuestions: number = 0;
-  rightAnswers: number = 0;
-  accuracy: number = 0;
+    pais: any = {};
+    answers: any[] = [];
+    numberOfQuestions: number = 0;
+    rightAnswers: number = 0;
+    accuracy: number = 0;
 
-  constructor(private http: HttpClient, private router: Router,
+    constructor(private http: HttpClient, private router: Router,
               private activatedRoute: ActivatedRoute,
               private paisService: PaisService) {
 
-      this.paisService.getRandomPais().subscribe(
-          (resp: any) => {
-              this.pais = resp[Math.floor(Math.random() * resp.length)];
-              console.log(this.pais);
-              this.answers.push(this.pais.capital);
-          }
-      );
+            this.getQuiz();
 
-      this.paisService.getRandomPais().subscribe(
-          (resp: any) => {
-              let i = 0;
-              for (i = 0; i < 2; i++) {
-                  this.answers.push(resp[Math.floor(Math.random() * resp.length)].capital);
-              }
+    }
 
-              console.log(this.answers);
-              this.answers = this.answers.sort(function() {return Math.random() - 0.5});
-              console.log(this.answers);
-          }
-      );
+    ngOnInit() {
+    }
 
-  }
+    getQuiz() {
+        this.paisService.getRandomPais().subscribe(
+            (resp: any) => {
+                this.pais = resp[Math.floor(Math.random() * resp.length)];
+                console.log(this.pais);
+                this.answers.push(this.pais.capital);
+            }
+        );
 
-  ngOnInit() {
-  }
+        this.paisService.getRandomPais().subscribe(
+            (resp: any) => {
+                let i = 0;
+                for (i = 0; i < 2; i++) {
+                    this.answers.push(resp[Math.floor(Math.random() * resp.length)].capital);
+                }
+
+                console.log(this.answers);
+                this.answers = this.answers.sort(function() {return Math.random() - 0.5});
+                console.log(this.answers);
+            }
+        );
+    }
 
     testAnswer(answer, rightAnswer) {
         this.numberOfQuestions ++;
         (answer === rightAnswer) ? this.rightAnswers++ : console.log('error');
 
         this.accuracy = Math.round(this.rightAnswers / this.numberOfQuestions * 100);
-        this.router.navigate(['/game']);
+        this.answers = [];
+        this.getQuiz();
         return false;
     }
 }
